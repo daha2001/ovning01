5607b92e47d2cc28c0525ce08dfa1aa2b3586bed
Daniel Håkansson <danielhakansson2@gmail.com>
2021-10-19 14:21:27 +0200

    lösning?

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        a = b;
+        b = 10;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }
